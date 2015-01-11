c71e413a213e8132092aa8c62138dae5cd61849d
Ryan Christensen <randhyllcho@gmail.com>
2015-01-10 17:30:02 -0800

    cited help

--- TweetieTime/NetworkController.swift
+++ TweetieTime/NetworkController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by RYAN CHRISTENSEN on 1/7/15.
 //  Copyright (c) 2015 RYAN CHRISTENSEN. All rights reserved.
-//
+//  fetchBackgroundBanner help from Daniel Hour
 
 import Foundation
 import Social
